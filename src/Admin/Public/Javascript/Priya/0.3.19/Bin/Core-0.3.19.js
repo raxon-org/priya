0b8471d8a6dc786202f26d0947a655633f861bde
Remco van der Velde <remco@universeorange.com>
2026-09-21 15:50:36 +0200

    init

--- src/Admin/Public/Javascript/Priya/0.3.19/Bin/Core-0.3.19.js
+++ src/Admin/Public/Javascript/Priya/0.3.19/Bin/Core-0.3.19.js
@@ -1942,7 +1942,7 @@ _('prototype').dump = function () {
  */
 _('prototype').exception = function (data, except){
     this.debug(JSON.stringify(data, null, 2));
-    let triggers = _('prototype').collection('debug.exception');
+    let triggers = this.collection('debug.exception');
     console.log(except);
     console.log(triggers);
     /*
